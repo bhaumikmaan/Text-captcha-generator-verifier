47904c833bac1e91d08f7dd4d88a3870f35542dc
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-07-20 00:10:21 +0530

    Create Generator.cpp

---
+++ Generator.cpp
@@ -0,0 +1,13 @@
+string GenerateCaptcha(int n)
+{
+	string GeneratedCaptcha = "" ;
+	string text = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+	srand(time(0)) ;
+	while(n--)
+	{
+		GeneratedCaptcha += text[rand()%62] ;
+	}
+	return GeneratedCaptcha ;
+}
+//Captcha String Length --> n
+//Characters used in Captcha --> Uppercase alphabets, lowercase alphabets & numbers
